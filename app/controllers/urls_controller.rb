55833e01fa4cc26ec6447d97fa2d514fadb78898
aeterne <aeterne@gmail.com>
2014-10-26 15:23:54 +0100

    add actions to controller

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,39 @@
+class UrlsController < ApplicationController
+	def index
+		@urls = Url.all
+	end
+
+	def show
+		url = Url.where(:random_id => params[:id]).first
+		if url
+			redirect_to url.original
+		else
+			render "index"
+		end
+	end
+
+# Creating the random string
+	def new
+		@url = Url.new
+
+		letters = [('a'..'z'),('A'..'Z')].map { |l| l.to_a }.flatter
+		@url.random_id = (0..8).map { letters[rand(letters.length)] }.join
+		
+	end
+
+	def url_params
+		params.require(:url).permit(:original, :random_id)
+	end
+
+	def create
+		@url = Url.new(url_params)
+		if @url.save
+			redirect_to urls_path
+		else
+			render 'new'
+		end
+	end
+
+
+
+end
